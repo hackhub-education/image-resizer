a22d18db0af1601dd098ceaf5cc26d1a41d48319
Yan Hong <onlyhongyan@gmail.com>
2018-05-15 17:24:59 -0700

    keep image aspect ratio

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const fs = require('fs');
 const resizeImg = require('resize-img');
+const sizeOf = require('image-size');
 
 const origDir = './images/orig/';
 const distDir = './images/dist/';
 
 const fileName = 'pic1.jpeg';
+
+const dimensions = sizeOf(origDir + fileName);
+const ratio = dimensions.width / dimensions.height;
+const distWidth = 128;
  
-resizeImg(fs.readFileSync(origDir + fileName), {width: 128, height: 128}).then(buf => {
+resizeImg(fs.readFileSync(origDir + fileName), {width: distWidth, height: distWidth / ratio}).then(buf => {
     fs.writeFileSync(distDir + fileName, buf);
 });
