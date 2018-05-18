d281bffb91c8c04296074016e60463a047eaa5ec
Michael Chen <cyjdyj@gmail.com>
2018-05-18 13:37:02 -0700

    add path

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
+const path = require('path');
 const resizeImg = require('resize-img');
 
-const origDir = './images/orig/';
-const distDir = './images/dist/';
+const origDir = path.join(__dirname, '/images/orig/');
+const distDir = path.join(__dirname, '/images/dist/');
 
 const fileName = 'pic1.jpeg';
  
-resizeImg(fs.readFileSync(origDir + fileName), {width: 128, height: 128}).then(buf => {
-    fs.writeFileSync(distDir + fileName, buf);
-});
+resizeImg(fs.readFileSync(origDir + fileName), { width: 128, height: 128 })
+    .then(buf => {
+        fs.writeFileSync(distDir + fileName, buf);
+    });
