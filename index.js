8bbe9c2d2c396e5fda127fc02fee46d394f06c62
Michael Chen <cyjdyj@gmail.com>
2018-05-18 14:58:41 -0700

    add server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const http = require('http');
 const resizeImg = require('resize-img');
 const sizeOf = require('image-size');
 
@@ -17,8 +18,14 @@ const fileName = 'pic1.jpeg';
 const dimensions = sizeOf(origDir + fileName);
 const ratio = dimensions.width / dimensions.height;
 const distWidth = 128;
- 
-resizeImg(fs.readFileSync(origDir + fileName), {width: distWidth, height: distWidth / ratio})
-    .then(buf => {
-        fs.writeFileSync(distDir + fileName, buf);
-    });
+
+http.createServer((req, res) => {
+    resizeImg(fs.readFileSync(origDir + fileName), {width: distWidth, height: distWidth / ratio})
+        .then(buf => {
+            fs.writeFileSync(distDir + fileName, buf);
+            res.write(buf); //write a response to the client
+            res.end(); //end the response
+        });
+  }).listen(8080); //the server object listens on port 8080
+  
+
