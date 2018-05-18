f4313cfa7efb38e98e539e179d55832189e803ed
Michael Chen <cyjdyj@gmail.com>
2018-05-18 14:04:54 -0700

    minor fix

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const resizeImg = require('resize-img');
 const origDir = path.join(__dirname, '/images/orig/');
 const distDir = path.join(__dirname, '/images/dist/');
 
+// create dist folder if it doesn't exist
+if (!fs.existsSync(distDir)){
+    fs.mkdirSync(distDir);
+}
+
 const fileName = 'pic1.jpeg';
- 
+
 resizeImg(fs.readFileSync(origDir + fileName), { width: 128, height: 128 })
     .then(buf => {
         fs.writeFileSync(distDir + fileName, buf);
     });
+console.log(new Buffer(10));
